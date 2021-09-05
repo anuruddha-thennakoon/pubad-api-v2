fac4a076366f6c45fcfbe4ca58b10e9c0bfeed8c
anuruddha.thennakoon <anuruddha.thennakoon@whitehatjr.com>
2021-09-05 08:59:10 +0530

    add local folder path

--- config/constants.js
+++ config/constants.js
@@ -2,8 +2,9 @@ var constants = {
     version: 'v1',
     appToken: 'DX9343ZXS9JPK5c8ws5ct9G4u3720jTQ5lHwbGJH777GflSQX1',
 
-    // FILE_PATH: '/home/slas-info/actions-runner/pubad-admin/pubad-admin-v2/pubad-admin-v2/dist/uploads',
-    FILE_PATH: '/home/slas-info/App/pubad-admin',
+    // FILE_PATH: '/home/slas-info/actions-runner/pubad-admin/pubad-admin-v2/pubad-admin-v2/dist/uploads',//auto build
+    FILE_PATH: '/home/slas-info/App/pubad-admin',//manual build
+    // FILE_PATH: './uploads',//local
 }
 
 module.exports = constants;
