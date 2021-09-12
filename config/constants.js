9c821ddc772339780051afc31a5518e0e8155c10
anuruddha.thennakoon <anuruddha.thennakoon@gmail.com>
2021-09-12 20:08:15 +0530

    change path

--- config/constants.js
+++ config/constants.js
@@ -3,7 +3,7 @@ var constants = {
     appToken: 'DX9343ZXS9JPK5c8ws5ct9G4u3720jTQ5lHwbGJH777GflSQX1',
 
     // FILE_PATH: '/home/slas-info/actions-runner/pubad-admin/pubad-admin-v2/pubad-admin-v2/dist/uploads',//auto build
-    FILE_PATH: '/home/slas-info/App/pubad-admin',//manual build
+    FILE_PATH: '/home/slas-info/App/pubad-admin/uploads',//manual build
     // FILE_PATH: './uploads',//local
 }
 
